ea01a51b5bf9a0b0f2c350b008f4179a99bb6dbf
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-09-27 21:09:13 +0100

    working on solution to exercise 5.7 from the c programminng language book

--- the-c-programming-language-book-exercises/5.7.c
+++ the-c-programming-language-book-exercises/5.7.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+* 5.7. Rewrite read lines to store lines in an array supplied by main,
+* rather than calling alloc to maintain storage. How much faster is the program?
+*/
+
 #define MAXLINES 5000
 
 char *lineptr[MAXLINES];
 
-int _getline(char *, int size);
+int get_line(char string[], int limit) {
+    int c, i;
+
+    for (i = 0; i < limit - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+        string[i] = c;
+    }
+
+    if (c == '\n') {
+        string[i] = c;
+        i = i + 1;
+    }
+
+    string[i] = '\0';
+
+    return i;
+}
+
 char *alloc(int);
 
 int readlines(char *lineptr[], int maxlines) {
@@ -14,7 +35,7 @@ int readlines(char *lineptr[], int maxlines) {
     const int MAXLEN = 1000;
     char *p, line[MAXLEN];
 
-    while ((len = _getline(line, MAXLEN)) > 0) {
+    while ((len = get_line(line, MAXLEN)) > 0) {
         if (nlines >= maxlines || (p = alloc(len)) == NULL) {
             return -1;
         }
@@ -33,7 +54,32 @@ void writelines(char *lineptr[], int nlines) {
     }
 }
 
-void qsort(char *lineptr[], int left, int right);
+void swap(char *value[], int i, int j) {
+    char *temp = value[i];
+    value[i] = value[j];
+    value[j] = temp;
+}
+
+void qsort(char *value[], int left, int right) {
+    int last;
+
+    if (left >= right) {
+        return;
+    }
+
+    swap(value, left, (left + right) / 2);
+    last = left;
+
+    for (int i = left + 1; i <= right; i++) {
+        if (strcmp(value[i], value[left]) < 0) {
+            swap(value, ++last, i);
+        }
+    }
+
+    swap(value, left, last);
+    qsort(value, left, last - 1);
+    qsort(value, last + 1, right);
+}
 
 int main() {
     int nlines;
